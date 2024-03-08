8c093c4cfd0036c18bd3888dcf206d7501501e33
Pritam <itspritammazumder@gmail.com>
2024-03-08 20:12:28 +0530

    project finished

---
+++ app/src/main/java/com/example/jcmeditationappui/ui/theme/Type.kt
@@ -0,0 +1,43 @@
+package com.example.jcmeditationappui.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.jcmeditationappui.R
+
+val gothicA1 = FontFamily(
+    listOf(
+        Font(R.font.gothica1_regular, FontWeight.Normal),
+        Font(R.font.gothica1_medium, FontWeight.Medium),
+        Font(R.font.gothica1_semibold, FontWeight.SemiBold),
+        Font(R.font.gothica1_bold, FontWeight.Bold),
+        Font(R.font.gothica1_black, FontWeight.Black)
+    )
+)
+
+val Typography = Typography(
+    // body1
+    bodyLarge = TextStyle(
+        color = AquaBlue,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    // h1
+    displayLarge = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    // h2
+    displayMedium = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    )
+)
